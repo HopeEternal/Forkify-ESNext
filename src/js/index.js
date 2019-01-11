a3bf5e6d203707062c7a94a6526a1aee9798e1bd
Risa <42893634+HopeEternal@users.noreply.github.com>
2019-01-10 18:20:47 -0700

    v0.2
    Pagination added.

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/**Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes 
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from the view
+    const query = searchView.getInput(); //TO Add to View
+    console.log(query);
+    
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI (only once promise is delivered)
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+
+});
